a20e7e03a51c2b206c87b982f664ae9b99a6ac42
Дмитрий Мельников <dima.dimix94@mail.ru>
2023-01-16 22:46:29 +0300

    refactor mongoose

--- src/helpers/create-app.ts
+++ src/helpers/create-app.ts
@@ -3,7 +3,7 @@ import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from '../exception.filter';
 
 export const createApp = (app: INestApplication) => {
